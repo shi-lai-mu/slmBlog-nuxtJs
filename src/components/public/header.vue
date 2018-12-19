084effae65838a78290b7a9ecd1f22871616ec0c
shi_lai_mu <478889187@qq.com>
2018-12-19 23:39:42 +0800

    header 转移

--- src/components/public/header.vue
+++ src/components/public/header.vue
@@ -20,3 +20,75 @@
     </header>
 
 </template>
+
+<style lang="less">
+
+header {
+    -webkit-user-select:none;
+    -moz-user-select:none;
+    -ms-user-select:none;
+    user-select:none;
+
+    .header-nav {
+        height: 75px;
+        list-style-type: none;
+        color: var(--color-mater-font);
+    }
+    .header-nav-left {
+        float: left;
+        margin-top: 5px;
+        height: 60px;
+        width: 100px;
+        background: url('~@img/ribbon-home.png') no-repeat;
+    
+        &:hover {
+            background-position-y: -68px;
+        }
+    }
+    .header-nav-right {
+        float: right;
+        width: 180px;
+        padding: 0 20px;
+        text-align: center;
+        font-size: 1.3em;
+        font-weight: bold;
+        color: var(--color-font-bg);
+        text-shadow: 1px 1px 0 #e06a5f;
+        line-height: 68px;
+        background: url('~@img/Work-With-Us.png') no-repeat top left;
+    }
+}
+
+// 顶部导航栏
+.header-nav-conter {
+    float: left;
+
+    li {
+        display: inline-block;
+        padding: 3px 0;
+        margin: 23px 5px;
+        width: 90px;
+        font-weight: bold;
+        text-align: center;
+        cursor: pointer;
+
+        &:hover, &:active {
+            background: url('~@img/nav-red-button.png') repeat-x center;
+            border-radius: 20px;
+            color: var(--color-font-bg);
+            text-shadow: 1px 1px 0 #e06a5f;
+            box-shadow: 1px 1px 2px #000;
+        }
+
+        &:active {
+            box-shadow: 0 0 2px #000;
+        }
+
+        .icon-fangxiangxia {
+            font-size: 12px;
+            margin: 0 5px;
+        }
+    }
+}
+
+</style>
