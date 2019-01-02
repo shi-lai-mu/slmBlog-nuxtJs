199cd4a96170e5a6f0e95dff244a4b6559d17eea
shi_lai_mu <478889187@qq.com>
2019-01-02 12:33:26 +0800

    修复2个BUG

--- src/components/public/header.vue
+++ src/components/public/header.vue
@@ -19,7 +19,7 @@
                     <span>{{ menu.tag }}</span>
                     <span v-if="menu.sub" class="iconfont icon-fangxiangxia"></span>
                     <ul v-if="menu.sub">
-                        <li v-for="(sub, n) in menu.sub" :key="n" v-if="!sub[2] || (sub[2] == 'login' && user) || (user && sub[2] == 'admin' && user.groupid === 9999)">
+                        <li v-for="(sub, n) in menu.sub" :key="n" v-if="!sub[2] || (sub[2] == 'login' && user) || (sub[2] == 'register' && !user) || (user && sub[2] == 'admin' && user.groupid === 9999)">
                             <router-link :to="sub[1]" class="max-a">{{ sub[0] }}</router-link>
                         </li>
                     </ul>
@@ -93,8 +93,8 @@ export default {
         {
           tag: '账号',
           sub: [
-            ['登录', 'login'],
-            ['注册', 'login?register'],
+            ['登录', 'login', 'register'],
+            ['注册', 'login?register', 'register'],
             ['管理账号', '#', 'login'],
             ['安全退出', '#', 'login']
           ]
