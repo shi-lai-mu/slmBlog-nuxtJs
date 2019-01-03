065990970106b190940e72e07219013d1de9bcf2
shi_lai_mu <478889187@qq.com>
2019-01-04 00:09:15 +0800

    空白前的显示

--- src/components/article/index.vue
+++ src/components/article/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="content-row single clearfix">
-    <div class="content-box article-index">
+    <div :class="['content-box', 'article-index', { 'notContent': !article['title'] }]">
       <header>
         <h2 class="article-title" v-text="article['title']"></h2>
         <h3 class="article-info">
-          <span>shilaimu 发表于：<i class="iconfont icon-shizhong" title="时间">{{ unTime(article['createTime']) }}</i></span>
+          <span>shilaimu 发表于：<i class="iconfont icon-shizhong" title="时间">{{ article['createTime'] && unTime(article['createTime']) }}</i></span>
           <span>
             <i class="iconfont icon-liaotian1" title="回复">{{ article['msgMe'] }}</i>
             <i class="iconfont icon-liulan" title="浏览">{{ article['lookCount'] }}</i>
           </span>
+          <ul v-if="!article['title']" class="notCon">
+            <li class="notP"></li>
+            <li></li>
+            <li class="notEnd"></li>
+            <li class="notP"></li>
+            <li></li>
+            <li></li>
+            <li class="notEnd"></li>
+            <li class="notP"></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li class="notEnd"></li>
+          </ul>
         </h3>
       </header>
       <div class="article-body" v-html="article['content']"></div>
@@ -44,26 +58,65 @@ export default {
 }
 </script>
 <style lang="less">
+  .notContent {
+    @bgColor: #d6d6d6;
+
+    .notCon {
+      width: 90%;
+      border-top: 1px solid #ccc;
+      margin: 40px auto 0;
+    }
+
+    .article-title {
+      width: 30%;
+      height: 1.5rem;
+      margin-left: auto;
+      margin-right: auto;
+      background-color: @bgColor;
+      border-radius: 1rem;
+      transition: 1s;
+    }
+    .notCon {
+      li {
+        width: 100%;
+        height: 1.5rem;
+        margin: 1.5rem auto;
+        background-color: @bgColor;
+        border-radius: 1rem;
+      }
+
+      .notP {
+        width: 90%;
+        margin-right: 0;
+      }
+      .notEnd {
+        width: 50%;
+        margin-left: 0;
+      }
+    }
+  }
   .article-index {
-    header {
+    .notContent header {
       width: 90%;
       border-bottom: 1px solid #ccc;
       margin: 0 auto 30px;
+    }
+    .article-title {
+      font-size: 2em;
+      text-align: center;
+    }
 
-      .article-title {
-        font-size: 2em;
-        text-align: center;
-      }
+    .article-info {
+      text-align: center;
+      font-weight: 400;
+      color: #555;
+    }
 
-      .article-info {
-        text-align: center;
-        font-weight: 400;
-        color: #555;
-      }
-
-      i {
-        margin: 0 5px 0 15px;
-        color: #888;
+    i {
+      margin: 0 5px 0 15px;
+      color: #888;
+      &::before {
+        margin-right: 5px;
       }
     }
     .article-body {
