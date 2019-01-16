1de8fdcc8db589d6acee89713ab9b506f8d21530
shi_lai_mu <478889187@qq.com>
2019-01-16 19:38:13 +0800

    修改样式4

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,6 +4,7 @@
 
         <!-- <shuffling></shuffling> -->
         <div class="content-row clearfix">
+
             <div class="content-box">
                 <router-link v-for="(hot, index) in hotList" :key="index" :to="'/article/' + hot.Id">
                     <div class="article clearfix">
@@ -12,7 +13,7 @@
                         </div>
                         <div class="article-right">
                             <h3 class="ellipsis">{{ hot.title }}</h3>
-                            <ul class="font-shadow-black">
+                            <ul class="article-tag">
                                 <li v-for="(type, i) in hot.type" :key="i" :title="'查找' + type + '标签'">
                                     <a href="#">{{ type }}</a>
                                 </li>
@@ -21,6 +22,21 @@
                         </div>
                     </div>
                 </router-link>
+                <ul v-if="!hotList.length">
+                    <!-- 展示伪装 -->
+                    <li v-for="i in 10" :key="i" class="article clearfix">
+                        <div class="article-left">
+                            <div class="no-black"></div>
+                        </div>
+                        <div class="article-right look-center">
+                            <h3 class="ellipsis no-black"></h3>
+                            <ul class="article-tag">
+                                <li v-for="i in 7" :key="i" class="no-black"></li>
+                            </ul>
+                            <p class="article-description no-black"></p>
+                        </div>
+                    </li>
+                </ul>
             </div>
 
             <div class="content-box content-aff-box">
@@ -45,6 +61,7 @@
 
 import Time from '@pub/js/dateForm'
 import shuffling from '@pub/vue/shuffling'
+import { setTimeout } from 'timers';
 
 export default {
   data () {
@@ -127,6 +144,7 @@ export default {
 
         h3 {
             display: inline-block;
+            padding: 10px 0;
             margin: 0;
             font-weight: 300;
         }
@@ -159,6 +177,36 @@ export default {
                 transform: scale(1.2);
             }
         }
+    }
+    .article-tag li {
+        display: inline-block;
+        padding: 0 5px;
+        border: 1px solid #e8e8e8;
+        border-radius: 3px;
+        margin: 0 5px;
+        vertical-align: middle;
+        font-size: .8rem;
+        color: #999;
+        transition: .3s;
+        &:hover {
+            background-color: #e9e9e9;
+            transform: translateY(-1px);
+        }
+    }
+    // 掩饰
+    .no-black {
+        min-width: 8rem;
+        border-radius: 20px;
+        margin: 10px 0;
+        background-color: #f1f1f1;
+    }
+    li.no-black {
+        min-width: 1rem;
+        height: 1rem;
+        border: 0;
+    }
+    .look-center {
+        margin-top: 20px;
     }
 }
 </style>
