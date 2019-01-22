5b35b4c5aed93f2fede0212237ec46a50cc3f0cd
shi_lai_mu <478889187@qq.com>
2019-01-22 22:14:08 +0800

    更新图标库

--- src/components/public/vue/music.vue
+++ src/components/public/vue/music.vue
@@ -2,7 +2,19 @@
   <div class="bottom-music">
     <!-- 底部浮动 -->
     <div class="bottom-music-float">
-      123456789
+      <i class="iconfont icon-up-copy"></i>
+|
+      <i class="iconfont icon-zanting"></i>
+      <i class="iconfont icon-zanting1"></i>
+      <i class="iconfont icon-next"></i>
+      <i class="iconfont icon-yinle"></i>
+      <i class="iconfont icon-xihuan1"></i>
+|
+      <i class="iconfont icon-shunxu1"></i>
+      <i class="iconfont icon-Shape"></i>
+      <i class="iconfont icon-shoucang"></i>
+      <i class="iconfont icon-suiji"></i>
+      <i class="iconfont icon-danquxunhuan"></i>
     </div>
   </div>
 </template>
@@ -24,6 +36,13 @@ export default {
       position: fixed;
       left: 0;
       bottom: 0;
+      padding: 10px;
+      border-radius: 5px;
+      background-color: #fff;
+      box-shadow: 0 0 10px rgba(0, 0, 0, .3);
+    }
+    i {
+      font-size: 1.5rem;
     }
   }
 </style>
