4f2a73b10c829bc389dea827c278ae141e73a158
shi_lai_mu <478889187@qq.com>
2019-02-03 11:07:09 +0800

    错误页修改

--- src/components/public/vue/error.vue
+++ src/components/public/vue/error.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="error">
-    <h2>404 页面内容失败</h2>
+    <h2>404</h2>
+    <h2>页面内容失败</h2>
   </div>
 </template>
 <style lang="less">
-  .error h2 {
-    color: #99A3B9;
-    font-size: 3rem;
-    text-align: center;
+  .error {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    h2 {
+      margin: 0 10px;
+      color: #99A3B9;
+      font-size: 3rem;
+      text-align: center;
+    }
   }
 </style>
