7a0b4efd605d314b83a602886816aba4762700d0
shi_lai_mu <478889187@qq.com>
2019-02-18 13:19:37 +0800

    修改css书写顺序6/兼容

--- src/components/public/vue/footer.vue
+++ src/components/public/vue/footer.vue
@@ -23,60 +23,82 @@
 <style lang="less">
 // 底部样式
 .body-footer {
-    margin-top: 20px;
-    padding-bottom: 20px;
-    color: #b6c3db;
-    user-select: none;
-    background-color: rgba(255, 255, 255, .8);
+  margin-top: 20px;
+  padding-bottom: 20px;
+  color: #b6c3db;
+  background-color: rgba(255, 255, 255, .8);
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+          user-select: none;
 
-    .copyright {
-        display: flex;
-        flex-direction: row;
+  .copyright {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    min-width: var(--min-width);
+    margin: 15px 0;
+    padding: 20px 10px 0;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+        -ms-flex-direction: row;
+            flex-direction: row;
+    -ms-flex-wrap: wrap;
         flex-wrap: wrap;
-        justify-content: center;
-        min-width: var(--min-width);
-        margin: 15px 0;
-        padding: 20px 10px 0;
-    }
-    .copyright-left {
-        width: 100%;
-        text-align: center;
-    }
-    .copyright-right {
-        margin-top: 5px;
-        li {
-            float: left;
-            &::after {
-                content: "|";
-                margin: 0 7px;
-            }
-        }
-        :nth-last-child(1) {
-            &::after {
-                content: "";
-            }
-        }
+    -webkit-box-pack: center;
+        -ms-flex-pack: center;
+            justify-content: center;
+  }
+
+  .copyright-left {
+    width: 100%;
+    text-align: center;
+  }
+
+  .copyright-right {
+    margin-top: 5px;
+
+    li {
+      float: left;
+      &::after {
+        content: "|";
+        margin: 0 7px;
+      }
     }
 
-    // 备案
-    .beian {
-        width: 100%;
-        min-width: var(--min-width);
-        margin: 0 auto;
-        .beian-a {
-            display: flex;
-            justify-content: center;
-            line-height: 20px;
-        }
-        .beian-img {
-            height: 20px;
-            display: inline-block;
-        }
-        .beian-p {
-            margin: 0 5px;
-            line-height: 20px;
-            color: #939393;
-        }
+    :nth-last-child(1) {
+      &::after {
+        content: "";
+      }
     }
+  }
+
+  // 备案
+  .beian {
+    width: 100%;
+    min-width: var(--min-width);
+    margin: 0 auto;
+
+    .beian-a {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      line-height: 20px;
+      -webkit-box-pack: center;
+          -ms-flex-pack: center;
+              justify-content: center;
+    }
+
+    .beian-img {
+      display: inline-block;
+      height: 20px;
+    }
+
+    .beian-p {
+      margin: 0 5px;
+      line-height: 20px;
+      color: #939393;
+    }
+  }
 }
 </style>
