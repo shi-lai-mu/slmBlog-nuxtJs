d8254f4234b10de96e5ae08f147e11b8d0cc29ba
shi_lai_mu <478889187@qq.com>
2019-02-20 10:50:38 +0800

    分离代码模型

---
+++ src/components/public/js/code-model.js
@@ -0,0 +1,27 @@
+export default {
+  javascript: {
+    // 关键词
+    'keyword': /\b(?:as|async|await|break|undefined|NaN|false|true|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|set|static|super|switch|this|throw|try|typeof|var|void|while|with|yield)\b/ig,
+    // 字符串
+    'string': /('[\s\S]*?')|(`[\s\S]*?`)|((?!class=)`[\s\S]*?`)/g,
+    // 注释
+    'annotation': /(\/\/[\s\S]*?\n)|(\/\/[\s\S]*?.*[^\n])|(\/\*\*)[\n\s\S]*?\*\//ig,
+    // 值
+    'value': /(\s+\d+(?!:'))|(\${\w+})|(\d+)/g,
+    // 'variable': /(\()\S+[\)]/,
+    // 符号
+    'operator': /((?!\*|&lt)[+\-%](?!>|=|\S+>|\(|\*))|(>=|>=|\/=|\*=|-=|\+=|\+\+|--)/g,
+    // 函数、调用
+    'function': /[_$a-z\xA0-\uFFFF][$\w\xA0-\uFFFF]*(?=\s*\()/ig,
+    // 返回值
+    'return': /\b(:?return)\b/g,
+    // 键
+    'key': /(this|\w+(?=\.))/g,
+    // 对象键
+    'key-obj': /(\w+(?=:))/g,
+    // 方法
+    'methods': /(\.\S+\(\))/g,
+    // 标签组
+    'html': /&lt[^>]*>|&lt\/[^>]*>/ig
+  }
+}
--- src/components/public/js/code.js
+++ src/components/public/js/code.js
@@ -1,4 +1,5 @@
 import '@pub/css/code.css'
+import codeModel from './code-model'
 
 export default {
 
@@ -23,33 +24,6 @@ export default {
 
 class Code {
   innerText = ''
-  regexp = {
-    // 关键词
-    'keyword': /\b(?:as|async|await|break|undefined|NaN|false|true|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|set|static|super|switch|this|throw|try|typeof|var|void|while|with|yield)\b/ig,
-    // 字符串
-    'string': /('[\s\S]*?')|(`[\s\S]*?`)|((?!class=)`[\s\S]*?`)/g,
-    // 注释
-    'annotation': /(\/\/[\s\S]*?\n)|(\/\/[\s\S]*?.*[^\n])|(\/\*\*)[\n\s\S]*?\*\//ig,
-    // 值
-    'value': /(\s+\d+(?!:'))|(\${\w+})|(\d+)/g,
-    // 'variable': /(\()\S+[\)]/,
-    // 符号
-    'operator': /((?!\*|&lt)[+\-%](?!>|=|\S+>|\(|\*))|(>=|>=|\/=|\*=|-=|\+=|\+\+|--)/g,
-    // 函数、调用
-    'function': /[_$a-z\xA0-\uFFFF][$\w\xA0-\uFFFF]*(?=\s*\()/ig,
-    // 返回值
-    'return': /\b(:?return)\b/g,
-    // 键
-    'key': /(this|\w+(?=\.))/g,
-    // 对象键
-    'key-obj': /(\w+(?=:))/g,
-    // 方法
-    'methods': /(\.\S+\(\))/g,
-    // 标签组
-    'html': /&lt[^>]*>|&lt\/[^>]*>/ig
-    // 净化污染 白色
-    // 'white': /[\(\),\.\:]/g
-  }
 
   constructor (element) {
     this.$el = element
@@ -83,7 +57,7 @@ class Code {
    * 替换语法高亮
    */
   replace () {
-    let regexp = this.regexp
+    let regexp = codeModel[this.model]
     let html = this.innerText
     // 排除标签
     html = this.innerText = html.replace(/&/gm, '&amp;').replace(/</gm, '&lt;')
