c15d796e3273e8850cc8474b11df3998e9e1a91d
shi_lai_mu <478889187@qq.com>
2019-03-02 18:54:40 +0800

    移植留言组件

--- src/components/article/index.vue
+++ src/components/article/index.vue
@@ -33,29 +33,9 @@
         </tbody>
 
         <!-- 尾部 -->
-        <footer :class="['article-body', { none: !article.title }]">
-          <!-- 留言 -->
-          <ul class="article-message" v-if="article.msg">
-            <span class="message-tag">
-              <span>留言</span>
-              <span class="message-right">({{ article.msg.all }})</span>
-            </span>
-            <!-- 留言内容 -->
-            <li v-for="(item, index) in article.msg.list" :key="index" class="clearfix">
-              <div class="clearfix">
-                <img src="//res.mczyzy.cn/img/user-default.jpg" alt="用户头像" class="user-icon">
-                <span class="user-name">{{ item.autherName }}</span>
-                <span class="user-info">
-                  {{ unTime(item.msgDate) }}
-                  <span class="message-right">{{ item.level }}楼</span>
-                </span>
-              </div>
-              <div class="message-content" v-html="item.content"></div>
-            </li>
-          </ul>
-          <Editor class="editor" ref="editor" model="send"></Editor>
-          <button class="button-lv0 send" @click="send">留言</button>
-        </footer>
+        <!-- <footer :class="['article-body', { none: !article.title }]"></footer> -->
+        <message :article="article" :unTime="unTime"></message>
+
     </section>
     <index-right :article="article" ref="artRight"></index-right>
   </tbody>
@@ -64,8 +44,8 @@
 <script>
 import Code from '@pub/js/code'
 import Time from '@pub/js/dateForm'
-import Editor from '@pub/vue/Editor'
 import indexRight from './index-right'
+import message from './message'
 
 export default {
   metaInfo () {
@@ -74,8 +54,8 @@ export default {
     }
   },
   components: {
-    Editor,
-    indexRight
+    indexRight,
+    message
   },
   data () {
     return {
@@ -131,7 +111,6 @@ export default {
           }
         })
         slef.article = data
-        console.log(data)
       })
       .catch(() => {
         this.$router.push({
@@ -149,23 +128,6 @@ export default {
   },
   methods: {
     unTime: time => Time.form('yyyy-MM-dd HH:mm:ss', time * 1000),
-
-    /* 发送留言 */
-    send () {
-      let user = this.$store.state.user
-      if (user) {
-        this.$http
-          .post('article/addMessage', {
-            id: this.$route.params.id,
-            token: user.token,
-            msg: this.$refs.editor.editorContent
-          })
-          .then(res => {
-            console.log(res)
-          })
-        this.$refs.editor.Stores.clear()
-      }
-    }
   }
 }
 </script>
@@ -359,92 +321,6 @@ export default {
       p {
         word-break: break-all;
       }
-    }
-
-    // 留言样式
-    .article-message {
-      padding: 0;
-
-      li {
-        padding: 10px;
-        list-style: none;
-        background-color: #eee;
-        border-radius: 5px;
-
-        .user-icon {
-          float: left;
-          margin-left: 0;
-        }
-      }
-
-      .message-tag {
-        position: relative;
-        display: block;
-        padding: 5px 10px;
-        color: #fff;
-        background-color: #f5d479;
-        border-top: 2px solid #f7be23;
-        border-radius: 0 5px 5px 0;
-
-        &::before {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 4px;
-          height: 100%;
-          background-color: #f7be23;
-          content: "";
-        }
-      }
-
-      .user-name,
-      .user-info {
-        float: left;
-        width: calc(100% - 40px);
-        overflow: hidden;
-        padding-left: 20px;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        -webkit-box-sizing: border-box;
-                box-sizing: border-box;
-      }
-
-      .user-info {
-        font-size: 1rem;
-        color: #aaa;
-      }
-
-      .message-content {
-        float: right;
-        width: calc(100% - 60px);
-        margin: 10px 5px;
-        padding: 10px;
-        background-color: rgba(255, 255, 255, .8);
-        border-radius: 5px;
-        word-wrap: break-word;
-      }
-
-      .message-right {
-        float: right;
-      }
-
-      p {
-        margin: 0;
-        text-indent: 0;
-      }
-    }
-
-    .editor .w-e-toolbar,
-    .editor .w-e-text-container {
-      width: 100%;
-    }
-
-    .send {
-      float: right;
-      width: 30%;
-      max-width: 150px;
-      margin: 20px;
-      border: 0;
     }
   }
 }
---
+++ src/components/article/message.vue
@@ -0,0 +1,149 @@
+<template>
+  <footer :class="['article-body', { none: !article.title }]">
+    <!-- 留言 -->
+    <ul class="article-message" v-if="article.msg">
+      <span class="message-tag">
+        <span>留言</span>
+        <span class="message-right">({{ article.msg.all }})</span>
+      </span>
+      <!-- 留言内容 -->
+      <li v-for="(item, index) in article.msg.list" :key="index" class="clearfix">
+        <div class="clearfix">
+          <img src="//res.mczyzy.cn/img/user-default.jpg" alt="用户头像" class="user-icon">
+          <span class="user-name">{{ item.autherName }}</span>
+          <span class="user-info">
+            {{ unTime(item.msgDate) }}
+            <span class="message-right">{{ item.level }}楼</span>
+          </span>
+        </div>
+        <div class="message-content" v-html="item.content"></div>
+      </li>
+    </ul>
+    <Editor class="editor" ref="editor" model="send"></Editor>
+    <button class="button-lv0 send" @click="send">留言</button>
+  </footer>
+</template>
+
+<script>
+import Editor from '@pub/vue/Editor'
+export default {
+  props: ['article', 'unTime'],
+  data () {
+    return {}
+  },
+  created () {
+    console.log(this)
+  },
+  components: {
+    Editor
+  },
+  methods: {
+    /* 发送留言 */
+    send () {
+      let user = this.$store.state.user
+      if (user) {
+        this.$http
+          .post('article/addMessage', {
+            id: this.$route.params.id,
+            token: user.token,
+            msg: this.$refs.editor.editorContent
+          })
+          .then(res => {
+            console.log(res)
+          })
+        this.$refs.editor.Stores.clear()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  .article-index {
+    // 留言样式
+    .article-message {
+      padding: 0;
+
+      li {
+        padding: 10px;
+        list-style: none;
+        background-color: #eee;
+        border-radius: 5px;
+
+        .user-icon {
+          float: left;
+          margin-left: 0;
+        }
+      }
+
+      .message-tag {
+        position: relative;
+        display: block;
+        padding: 5px 10px;
+        color: #fff;
+        background-color: #f5d479;
+        border-top: 2px solid #f7be23;
+        border-radius: 0 5px 5px 0;
+
+        &::before {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 4px;
+          height: 100%;
+          background-color: #f7be23;
+          content: "";
+        }
+      }
+
+      .user-name,
+      .user-info {
+        float: left;
+        width: calc(100% - 40px);
+        overflow: hidden;
+        padding-left: 20px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        -webkit-box-sizing: border-box;
+                box-sizing: border-box;
+      }
+
+      .user-info {
+        font-size: 1rem;
+        color: #aaa;
+      }
+
+      .message-content {
+        float: right;
+        width: calc(100% - 60px);
+        margin: 10px 5px;
+        padding: 10px;
+        background-color: rgba(255, 255, 255, .8);
+        border-radius: 5px;
+        word-wrap: break-word;
+      }
+
+      .message-right {
+        float: right;
+      }
+
+      p {
+        margin: 0;
+        text-indent: 0;
+      }
+    }
+
+    .editor .w-e-toolbar,
+    .editor .w-e-text-container {
+      width: 100%;
+    }
+
+    .send {
+      float: right;
+      width: 30%;
+      max-width: 150px;
+      margin: 20px;
+      border: 0;
+    }
+  }
+</style>
