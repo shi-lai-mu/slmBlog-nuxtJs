e6dbd6b59568e5ab692029b5c47615b02af8c043
shi_lai_mu <478889187@qq.com>
2019-03-17 15:45:53 +0800

    修改切换动画、部分个人中心代码

--- pages/user/ment/hot.vue
+++ pages/user/ment/hot.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    ss
+    制作中...
   </div>
 </template>
--- pages/user/ment/new.vue
+++ pages/user/ment/new.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="article">
-
-    <ul class="article-list">
-      <li v-for="(item, index) in hot" :key="index">
-        <img :src="item.img" :alt="item.title">
-        <div class="ellipsis article-info">
-          <router-link class="max-a" :to="{ name: 'article-id', params: { id: item.Id } }">
-            <div class="ellipsis article-list-title" v-text="item.title"></div>
-            <div class="ellipsis article-list-desc" v-text="item.description"></div>
-            <div class="article-list-time">{{ item.createTime }}</div>
-          </router-link>
-        </div>
-      </li>
-    </ul>
-  </div>
+  <ul class="article-list">
+    <li v-for="(item, index) in hot" :key="index">
+      <img :src="item.img" :alt="item.title">
+      <div class="ellipsis article-info">
+        <router-link class="max-a" :to="{ name: 'article-id', params: { id: item.Id } }">
+          <div class="ellipsis article-list-title" v-text="item.title"></div>
+          <div class="ellipsis article-list-desc" v-text="item.description"></div>
+          <div class="article-list-time">{{ item.createTime }}</div>
+        </router-link>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -36,39 +33,37 @@ export default {
 </script>
 
 <style lang="less">
-.user-center .article {
-  .article-list {
-    li {
-      display: flex;
-      padding: 10px 0;
-      border-bottom: 1px solid #eee;
-      align-items: center;
+.user-center .article-list {
+  li {
+    display: flex;
+    padding: 10px 0;
+    border-bottom: 1px solid #eee;
+    align-items: center;
+  }
+
+  img {
+    width: 20%;
+    max-width: 100px;
+    margin-right: 10px;
+    border-radius: 5px;
+  }
+
+  .article-info {
+    width: 100%;
+
+    .article-list-title {
+      font-size: 1.2rem;
     }
 
-    img {
-      width: 20%;
-      max-width: 100px;
-      margin-right: 10px;
-      border-radius: 5px;
+    .article-list-desc {
+      color: #aaa;
     }
 
-    .article-info {
-      width: 100%;
-
-      .article-list-title {
-        font-size: 1.2rem;
-      }
-
-      .article-list-desc {
-        color: #aaa;
-      }
-
-      .article-list-time {
-        float: right;
-        margin-top: 5px;
-        font-size: .8rem;
-        color: #ccc;
-      }
+    .article-list-time {
+      float: right;
+      margin-top: 5px;
+      font-size: .8rem;
+      color: #ccc;
     }
   }
 }
