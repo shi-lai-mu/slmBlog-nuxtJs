c705e8a6836e9da84f8e0bd8a80bbd98acbbd64d
史莱姆 <478889187@qq.com>
2019-06-30 20:47:58 +0800

    @dev 留言大体

--- pages/leaves/message.vue
+++ pages/leaves/message.vue
@@ -1,16 +1,27 @@
 <template>
-  <section class="conter single clearfix leaves">
+  <section class="conter single clearfix leaves article-body">
     <div class="content-box">
       <h2 class="title">留言板</h2>
       <img class="cover" src="/img/messgae-nav.jpg" alt="史莱姆的博客-留言板-封面">
+      <blockquote>欢迎来到史莱姆的博客，尽情的留下你的脚印吧！</blockquote>
+    </div>
+    <div class="content-box">
+      <div>留言共计 () 条</div>
+      <editor />
     </div>
   </section>
 </template>
 
 <script>
+import Editor from '~/plugins/Editor'
+
 export default {
   created() {
     console.log(12345674897)
+  },
+
+  components: {
+    Editor
   }
 }
 </script>
@@ -22,11 +33,25 @@ export default {
     font-weight: 400;
     text-align: center;
   }
+
   img.cover {
     display: inline-block;
     width: 90%;
     margin: 0 5%;
+    border: 5px solid #eee;
     border-radius: 10px;
+  }
+
+  .content-box blockquote {
+    display: block;
+    width: 90%;
+    padding: 5px 10px;
+    margin: 10px auto;
+    line-height: 1.4;
+    font-size: 100%;
+    background-color: #eee;
+    border-left: 8px solid #ccc;
+    border-radius: 0 5px 5px 0;
   }
 }
 </style>
