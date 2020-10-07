dd8cd7f9529afd1ac25133294d49c3c6005c4992
shi-lai-mu <478889187@qq.com>
2020-10-07 08:18:04 +0800

    fix: 修复less script导致scroll失效

--- static/script/index.js
+++ static/script/index.js
@@ -1,4 +1,11 @@
+/* tslint:disable */
 window.less = {
   async: false,
   env: 'production'
 };
+
+setTimeout(() => {
+  var less = document.createElement('script');
+  less.src = '/script/less.min.js';
+  document.body.append(less);
+});
