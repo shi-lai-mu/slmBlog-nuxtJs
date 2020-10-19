cd1254591961a68b17f72239029c28dec9abe2f5
shi-lai-mu <478889187@qq.com>
2020-10-20 00:00:47 +0800

    feat: 公共化 公告、用户卡片 组件

---
+++ components/public/UserCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- 用户信息展示卡片 -->
+  <div class="row-box user-card">
+    <div class="user-cover" :style="`background-image: url(${$config.ossLink}/user/card-bg-cover.jpg);`">
+      <img class="user-avatar" src="//thirdqq.qlogo.cn/g?b=sdk&k=s3zxCIMMOxfQibT9H8la8zg&s=100?v=1603024316672" alt="">
+    </div>
+    <div class="row-content">
+      content
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator';
+
+@Component
+export default class UserCard extends Vue {
+  /**
+   * 展示的用户ID
+   */
+  @Prop(Number) userId?: number;
+  
+}
+</script>
+
+<style scoped lang="scss">
+  .user-card {
+    overflow: hidden;
+    padding: 0;
+  }
+  .user-cover {
+    min-height: 150px;
+    
+    .user-avatar {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width: 80px;
+      height: 80px;
+      margin: auto;
+      border-radius: 50%;
+    }
+  }
+</style>
