5f8fa1fac7c3cdb64879b640638b5e55f6e0073e
shi-lai-mu <478889187@qq.com>
2020-10-29 10:51:32 +0800

    feat: 用户卡组件增加骨架屏

--- components/public/UserCard.vue
+++ components/public/UserCard.vue
@@ -1,33 +1,36 @@
 <template>
   <!-- 用户信息展示卡片 -->
-  <div class="row-box user-card" v-if="userData.id">
-    <div class="user-cover" :style="`background-image: url(${$config.ossLink}/user/card-bg-cover.jpg);`">
-      <img class="user-avatar" :src="userData.avatarUrl" :alt="userData.nickname" :title="userData.nickname">
-    </div>
-    <div class="row-content">
-      <div class="user-nickname">
-        {{ userData.nickname }}
-        <i :class="['slm', 'blog-' + item.i]" :title="item.name" v-for="(item, key) in userData.badge" :key="key"></i>
+  <div class="row-box user-card">
+    <template v-if="userData.id">
+      <div class="user-cover" :style="`background-image: url(${$config.ossLink}/user/card-bg-cover.jpg);`">
+        <img class="user-avatar" :src="userData.avatarUrl" :alt="userData.nickname" :title="userData.nickname">
       </div>
-      <div class="user-introduction">{{ userData.introduction }}</div>
-      <div class="user-state-row">
-        <span class="stete-item" v-for="(item, index) in showState" :key="index">
-          <div class="state-item-tag">{{ item }}</div>
-          <div>{{ userData.state[index] }}</div>
-        </span>
+      <div class="row-content">
+        <div class="user-nickname">
+          {{ userData.nickname }}
+          <i :class="['slm', 'blog-' + item.i]" :title="item.name" v-for="(item, key) in userData.badge" :key="key"></i>
+        </div>
+        <div class="user-introduction">{{ userData.introduction }}</div>
+        <div class="user-state-row">
+          <span class="stete-item" v-for="(item, index) in showState" :key="index">
+            <div class="state-item-tag">{{ item }}</div>
+            <div>{{ userData.state[index] }}</div>
+          </span>
+        </div>
+        <div class="user-icon">
+          <a
+            target="_blank"
+            v-for="(item, index) in showIcon"
+            v-show="item.link(userData)"
+            :key="index"
+            :class="[ 'slm', item.icon ]"
+            :href="item.link(userData)"
+            :title="`访问 ${userData.nickname} 的 ${item.title}`"
+          ></a>
+        </div>
       </div>
-      <div class="user-icon">
-        <a
-          target="_blank"
-          v-for="(item, index) in showIcon"
-          v-show="item.link()"
-          :key="index"
-          :class="[ 'slm', item.icon ]"
-          :href="item.link()"
-          :title="`访问 ${userData.nickname} 的 ${item.title}`"
-        ></a>
-      </div>
-    </div>
+    </template>
+    <UserCardSkeleton v-else />
   </div>
 </template>
 
@@ -36,8 +39,13 @@ import { Component, Vue, Prop, Watch }   from 'nuxt-property-decorator';
 import { User } from '@/interface/user/index';
 import { userData } from '@/mock/user/data/user.ts';
 import { getUserBaseData } from '@/service/data/user.ts';
+import UserCardSkeleton from '../skeleton/pubCom/userCardSkeleton.vue';
 
-@Component
+@Component({
+  components: {
+    UserCardSkeleton,
+  },
+})
 export default class UserCard extends Vue {
   /**
    * 展示的用户ID
@@ -70,22 +78,22 @@ export default class UserCard extends Vue {
     blog: {
       title: '个人博客',
       icon: 'blog-link',
-      link: () => this.userData?.link.blog,
+      link: v => v?.link?.blog,
     },
     twitter: {
       title: 'Twitter',
       icon: 'blog-twitter',
-      link: () => this.userData?.link.twitter,
+      link: v => v?.link.twitter,
     },
     github: {
       title: 'GitHub',
       icon: 'blog-github',
-      link: () => this.userData?.link.github,
+      link: v => v?.link.github,
     },
     email: {
       title: '邮箱',
       icon: 'blog-email',
-      link: () => this.userData?.email,
+      link: v => v?.email,
     }
   }
 
@@ -97,9 +105,7 @@ export default class UserCard extends Vue {
   userIdUpate(userId: number) {
     getUserBaseData(userId)
       .then(data => {
-        if (data.result) {
-          this.userData = data.result;
-        }
+        if (data.result) this.userData = data.result;
       })
     ;
   }
@@ -125,52 +131,52 @@ export default class UserCard extends Vue {
   .user-card {
     overflow: hidden;
     padding: 0;
+  }
 
-    .row-content {
-      padding: 35px 25px 10px;
-      font-size: .7rem;
-      text-align: center;
+  .user-card /deep/ .row-content {
+    padding: 35px 25px 10px;
+    font-size: .7rem;
+    text-align: center;
 
-      .user-nickname {
-        font-size: 1.2rem;
-        font-weight: 500;
+    .user-nickname {
+      font-size: 1.2rem;
+      font-weight: 500;
+    }
+
+    .user-introduction {
+      margin-top: 10px;
+    }
+
+    .user-state-row {
+      display: flex;
+      margin-top: 20px;
+
+      .stete-item {
+        width: 100%;
+        font-size: .8rem;
       }
 
-      .user-introduction {
-        margin-top: 10px;
-      }
-
-      .user-state-row {
-        display: flex;
-        margin-top: 20px;
-
-        .stete-item {
-          width: 100%;
-          font-size: .8rem;
-        }
-
-        .state-item-tag {
-          @include themify($themes) {
-            color: themed('font-lv2-color');
-          }
+      .state-item-tag {
+        @include themify($themes) {
+          color: themed('font-lv2-color');
         }
       }
+    }
 
-      .user-icon {
-        display: flex;
-        margin-top: 20px;
-        justify-content: center;
+    .user-icon {
+      display: flex;
+      margin-top: 20px;
+      justify-content: center;
 
-        .slm {
-          display: block;
-          margin: 0 10px;
-          text-decoration: none;
-        }
+      .slm {
+        display: block;
+        margin: 0 10px;
+        text-decoration: none;
       }
     }
   }
 
-  .user-cover {
+  .user-card /deep/ .user-cover {
     position: relative;
     min-height: 150px;
     background-repeat: no-repeat;
---
+++ components/skeleton/pubCom/userCardSkeleton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="skeleton">
+    <div class="user-cover">
+      <span class="user-avatar"></span>
+    </div>
+    <div class="row-content">
+      <span class="user-nickname skeleton-title skeleton-center"></span>
+      <span class="user-introduction skeleton-desc skeleton-center"></span>
+      <div class="user-state-row">
+        <ul class="stete-item" v-for="(item, index) in 3" :key="index">
+          <li class="state-item-tag skeleton-tag skeleton-center"></li>
+          <li class="skeleton-tag skeleton-center"></li>
+        </ul>
+      </div>
+      <ul class="user-icon">
+        <span v-for="(item, index) in 4" :key="index" class="slm skeleton-icon skeleton-center"></span>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue }   from 'nuxt-property-decorator';
+/**
+ * 用户卡骨架
+ */
+@Component
+export default class UserCardSkeleton extends Vue {}
+</script>
