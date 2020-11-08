d7c8275799534de6e3cd9a0711dd80ecacfda465
shi-lai-mu <478889187@qq.com>
2020-11-08 13:15:50 +0800

    feat: 增加骨架屏、增加文章路由页

---
+++ components/skeleton/pubCom/articleViewSkeleton.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="skeleton">
+    <a-row class="article-layout max-content">
+      <a-col
+        class="article-page__container max-content"
+        :lg="{ span: 16 }"
+        :xxl="{ span: 16 }"
+      >
+        <aside class="article-action"></aside>
+        <div class="article-content__container row-box">
+          <div class="article-content__header">
+            <span class="title skeleton-title"></span>
+            <div class="article-content__info">
+              <span class="release-time skeleton-desc"></span>
+              <div class="icon-box">
+                <span class="skeleton-tag"></span>
+                <span class="skeleton-tag"></span>
+              </div>
+              <ul class="tag-list">
+                <li class="tag-item skeleton-tag" v-for="(item, key) in 5" :key="key"></li>
+              </ul>
+            </div>
+          </div>
+          <div class="article-content__body">
+            <ul class="skeleton-content-box" v-for="(item, key) in 3" :key="key">
+              <li class="skeleton-title"></li>
+              <li class="skeleton-desc"></li>
+              <li class="skeleton-desc"></li>
+              <li class="skeleton-desc"></li>
+              <li class="skeleton-desc"></li>
+            </ul>
+          </div>
+        </div>
+        <div class="article-reply__container">
+
+        </div>
+      </a-col>
+      <a-col
+        class="article-page__sideber"
+        :lg="{ span: 8 }"
+        :xxl="{ span: 8 }"
+      >
+        <UserCard />
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue }   from 'nuxt-property-decorator';
+import UserCard from '@/components/public/UserCard.vue';
+
+/**
+ * 文章内容骨架
+ */
+@Component({
+  components: {
+    UserCard,
+  },
+})
+export default class UserCardSkeleton extends Vue {}
+</script>
+
+<style scoped lang="scss">
+  .skeleton {
+    position: absolute;
+    z-index: 999;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    .article-layout {
+      opacity: 1 !important;
+    }
+
+    .article-content__header .skeleton-title {
+      font-size: 2rem;
+    }
+
+    .skeleton-tag {
+      width: 50px;
+    }
+
+    .skeleton-content-box {
+      font-size: 1.5rem;
+      .skeleton-title {
+        margin-bottom: 25px;
+      }
+
+      .skeleton-desc {
+        margin-bottom: 20px;
+      }
+
+      & :nth-child(2n) {
+        width: 90% !important;
+      }
+    }
+  }
+</style>
---
+++ pages/article/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <ArticleContent :ssr="articleData[0]"/>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+import { getLatestArticle } from '@/service/data/article';
+
+import ArticleContent from '@/components/public/Article/Contents.vue';
+
+@Component({
+  components: {
+    ArticleContent,
+  },
+})
+export default class HomePage extends Vue {
+  async asyncData({ $http, $axios}) {
+    return {
+      articleData: (await getLatestArticle()).result || [], // 获取最新文章
+    };
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
