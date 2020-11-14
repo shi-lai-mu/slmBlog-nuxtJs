6a6553e70a398594d2b142feb9ee453df46ae2e1
shi-lai-mu <478889187@qq.com>
2020-11-14 20:43:05 +0800

    feat: 焦点板块

---
+++ components/pages/home/FocalPointPlate.vue
@@ -0,0 +1,94 @@
+<template>
+  <FunctionalPlate title="焦点推荐">
+    <div class="plate-first-place__row">
+      <Imager class="plate-first-place__img" :src="'http://dummyimage.com/336x280'"/>
+      <p class="plate-first-place__title line-ellipsis">这是一个首板块标题块标题块标题块</p>
+    </div>
+    <div class="plate-list-place">
+      <nuxt-link :to="'#'" class="plate-list-opt__link" v-for="k in 4" :key="k">
+        <label class="opt-link__label">活动</label>
+        <p class="opt-link__title line-ellipsis">这是一个链接 哈哈哈哈哈哈哈哈哈哈或或或或或或或或或或或或或或或或或或或</p>
+      </nuxt-link>
+    </div>
+  </FunctionalPlate>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+
+import Imager from '@/components/public/Imager.vue';
+import FunctionalPlate from '@/components/public/FunctionalPlate.vue';
+
+/**
+ * 焦点板块 组件
+ */
+@Component({
+  name: 'FocalPointPlate',
+  components: {
+    FunctionalPlate,
+    Imager,
+  },
+})
+export default class FocalPointPlate extends Vue {
+  @Prop(String) ssr;
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.plate-first-place__row {
+  overflow: hidden;
+  position: relative;
+  border-radius: 10px;
+  cursor: pointer;
+
+  .plate-first-place__img {
+    height: 150px;
+  }
+
+  .plate-first-place__title {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    margin-bottom: 0;
+    padding: 5px 10px;
+    background-color: rgba($color: #000, $alpha: .45);
+    transition: .5s;
+
+    &:hover {
+      background-color: rgba($color: #000, $alpha: .6);
+    }
+  }
+}
+.plate-list-place {
+  padding: 10px 0;
+  .plate-list-opt__link {
+    display: inline-flex;
+    width: 100%;
+    margin-top: 10px;
+    white-space: nowrap;
+    align-items: center;
+
+    .opt-link__label {
+      width: 32px;
+      height: 16px;
+      line-height: 16px;
+      margin-right: 20px;
+      font-size: 12px;
+      color: #FFF;
+      background-color: #C3CCD9;
+      border-radius: 3px;
+      text-align: center;
+      flex-shrink: 0;
+    }
+
+    .opt-link__title {
+      margin-bottom: 0;
+    }
+
+    & + .plate-list-place__card {
+      margin-top: 10px;
+    }
+  }
+}
+</style>
---
+++ components/public/FunctionalPlate.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="row-box functional-plate__row">
+    <div class="functional-plate__header">
+      <slot name="header">
+        <h2 class="functional-plate__title" v-text="title || '板块'"></h2>
+        <nuxt-link :to="moreLink || '#'" class="functional-plate__link" v-text="moreName || '更多'"></nuxt-link>
+      </slot>
+    </div>
+    <div class="functional-plate__body">
+      <slot>
+        <div class="functional-plate__loading">loading ...</div>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+
+/**
+ * 工具板块组件
+ */
+@Component({ name: 'PublicFunctionalPlateComponent' })
+export default class PublicFunctionalPlateComponent extends Vue {
+  /**
+   * 标题
+   */
+  @Prop(String) title;
+  /**
+   * 更多 文字内容
+   */
+  @Prop(String) moreName;
+  /**
+   * 更多 链接
+   */
+  @Prop(String) moreLink;
+}
+</script>
+
+<style lang="scss" scoped>
+  .functional-plate__row {
+    padding: 0;
+
+    .functional-plate__header {
+      display: flex;
+      padding: 15px 20px;
+      @include themify($themes) {
+        border-bottom: 1px solid themed('border-bottom-color');
+      }
+      justify-content: space-between;
+      align-items: center;
+
+      .functional-plate__title {
+        margin-bottom: 0;
+        font-size: 1.05rem;
+        font-weight: bold;
+        @include themify($themes) {
+          color: themed('a-color');
+        }
+      }
+    }
+
+    .functional-plate__body {
+      padding: 15px;
+
+      .functional-plate__loading {
+        text-align: center;
+        @include themify($themes) {
+          color: themed('font-lv3-color');
+        }
+      }
+    }
+  }
+</style>
